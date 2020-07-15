160b8667b03bc04fd37c811013f39c39cb269e33
jorge <jootca@gmail.com>
2020-07-15 22:13:22 +0200

    calcu 15 julio 2020

--- js/actividad.js
+++ js/actividad.js
@@ -10,12 +10,14 @@
  function mostrar(a) {
 
 
- 	var b = document.getElementById("pantalla").getAttribute("value");
+	 var b = document.getElementById("pantalla").getAttribute("value");
+	 console.log(signo);
+	 console.log(decimal);
  	//inicio(primer dato)
  	if (b == 0 && ceroDeReseteo) {
 
  		//casos 1 dato signos
- 		if (a == "*" || a == "/" || a == "+" || a == "-" || a == ".") {
+ 		if (a == "*" || a == "/" || a == "+" || a == ".") {
  			if (a == ".") {
 
  				signo = true;
@@ -28,8 +30,17 @@
  				return;
 
  			}
+
  			return;
- 		}
+		 }
+		 if (a == "-") {
+			ceroDeReseteo = false;
+			signo = true;
+			
+			document.getElementById("pantalla").setAttribute("value", a);
+			return;
+		 }
+ 		
  		//caso numeros
  		ceroDeReseteo = false;
  		signo = false;
@@ -106,6 +117,7 @@
  			return;
  		}
  		if (signo && !decimal) {
+			
  			if (a == "." && !(a == "*" || a == "/" || a == "+" || a == "-")) {
  				return;
  			}
@@ -115,8 +127,10 @@
  				decimal = true;
  				document.getElementById("pantalla").setAttribute("value", b);
 
- 			}
-
+			 }
+			 
+			 
+            
 
  			return;
  		}
@@ -128,11 +142,19 @@
  	if (operandoString != "") {
 
  		return;
- 	}
+	 }
+
+	 
+	 if (memoriaPantalla && b.slice(-1)=="-") {
+		signo = false;
+		b += a;
+		document.getElementById("pantalla").setAttribute("value", b);
+	 }
  	if (memoriaPantalla) {
  		return;
  	}
- 	console.log("aqui<br>");
+	 console.log("aqui<br>");
+	 console.log("aqui" + a);
  	signo = false;
  	b += a;
  	document.getElementById("pantalla").setAttribute("value", b);
@@ -141,15 +163,15 @@
  function resetear() {
  	document.getElementById("pantalla").setAttribute("value", "0");
  	signo = false;
- 	ceroDeReseteo = true;
  	decimal = false;
  	contador = 0;
-
+ 	ceroDeReseteo = true;
  }
 
  function borrarMemoria() {
  	operandoMemoria = "";
  	memoriaPantalla = false;
+ 	document.getElementById("mem").setAttribute("value", "");
 
  }
 
@@ -158,22 +180,43 @@
 
 
  	if (operandoMemoria == "") {
- 		//preparo valor seguro
- 		b = eval(b);
- 		//si esta vacio obtengo valor en variable de memoria
- 		operandoMemoria = b;
- 		memoriaPantalla = true;
- 		signo = false;
 
- 		resetear();
+ 		if (b.slice(-1) == "*" || b.slice(-1) == "/" || b.slice(-1) == "+" || b.slice(-1) == "-" || b.slice(-1) == ".") {
+ 			//me quedo con el valor seguro eliminando el signo
+ 			b = b.slice(0, -1);
+ 			//preparo valor seguro
+ 			b = eval(b);
+ 			//si esta vacio obtengo valor en variable de memoria
+ 			operandoMemoria = b;
+ 			//lo envio a pantalla de memoria
+ 			document.getElementById("mem").setAttribute("value", b);
+ 			resetear();
+ 			memoriaPantalla = true;
+ 		} else {
+
+ 			b = eval(b);
+ 			operandoMemoria = b;
+ 			document.getElementById("mem").setAttribute("value", b);
+ 			resetear();
+ 			memoriaPantalla = true;
+
+ 		}
+
+
+
  	} else {
  		//1º caso se supone que no puede entrar signo
  		if (ceroDeReseteo) {
  			document.getElementById("pantalla").setAttribute("value", operandoMemoria);
+ 			ceroDeReseteo = false;
 
  		} else {
  			console.log(operandoMemoria);
+ 			//si tiene signo antes se puede poner numero en valor seguro
  			if (b.slice(-1) == "*" || b.slice(-1) == "/" || b.slice(-1) == "+" || b.slice(-1) == "-") {
+ 				if (b.slice(-1) == "-" && operandoMemoria < 0) {
+ 					b = b.slice(0, -1);
+ 				}
  				console.log("entro");
  				b = b + operandoMemoria;
  				document.getElementById("pantalla").setAttribute("value", b);
@@ -204,21 +247,31 @@
 
  function invertir() {
  	var b = document.getElementById("pantalla").getAttribute("value");
- 	if (b.charAt(0) == "-") {
-
- 		var elimina = b.replace(/\*/g, "");
- 		var elimina = elimina.replace(/\//g, "");
- 		var elimina = elimina.replace(/\+/g, "");
- 		var elimina = elimina.replace(/-/g, "");
- 		document.getElementById("pantalla").setAttribute("value", elimina);
+ 	//va a se un valor seguro eliminando algun signo que pueda hacer fallar el eval
+ 	if (b.slice(-1) == "*" || b.slice(-1) == "/" || b.slice(-1) == "+" || b.slice(-1) == "-" || b.slice(-1) == ".") {
+ 		//me quedo con el valor seguro eliminando el signo
+ 		b = b.slice(0, -1);
+ 		//lo evalue 
+ 		b = eval(b);
+ 		//invierto su valor ya que es un boton de inversion
+ 		document.getElementById("pantalla").setAttribute("value", -b);
+ 		//muevo su valor a la variable resultado para impedir la intrusion de un numero o del signo "." hasta que no se meta un operando
+ 		operandoString = -b;
+ 		//reseteo variables a inicio salvo ceroDeReseteo ya que tenemos un valor puesto
+ 		signo = false;
+ 		decimal = false;
+ 		contador = 0;
+ 		ceroDeReseteo = false;
  	} else {
+ 		//opcion en la que llega valor seguro misma lógica que la anterior
+ 		b = eval(b);
+ 		document.getElementById("pantalla").setAttribute("value", -b);
+ 		operandoString = -b;
+ 		signo = false;
+ 		decimal = false;
+ 		contador = 0;
+ 		ceroDeReseteo = false;
 
- 		var elimina = b.replace(/\*/g, "");
- 		var elimina = elimina.replace(/\//g, "");
- 		var elimina = elimina.replace(/\+/g, "");
- 		var elimina = elimina.replace(/-/g, "");
- 		document.getElementById("pantalla").setAttribute("value", eval(-elimina));
  	}
-
 
  }
